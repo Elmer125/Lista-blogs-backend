f5735b590da8fdb87e52a875ebeb0e7dc49a2ada
ELMER MAURICIO MARULANDA OSORIO <elmer2164701@correo.uis.edu.co>
2022-02-16 18:25:52 -0500

    update backend conectado a base de datos de mongodb

---
+++ controllers/blog-list.js
@@ -0,0 +1,26 @@
+const blogRouter = require("express").Router();
+const Blog = require("../models/blogList");
+
+blogRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+blogRouter.post("/", (request, response, next) => {
+  const body = request.body;
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  });
+  blog
+    .save()
+    .then((savedBlog) => {
+      response.json(savedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = blogRouter;
